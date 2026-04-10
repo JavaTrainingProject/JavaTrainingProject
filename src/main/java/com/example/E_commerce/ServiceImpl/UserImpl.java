dd29572e531ac3ed0586a4e1206c181bf4d1561c
AjayGudduru <gayathri.b@clientservertech.com>
2026-04-10 12:31:47 +0530

    my changes332.3

--- src/main/java/com/example/E_commerce/ServiceImpl/UserImpl.java
+++ src/main/java/com/example/E_commerce/ServiceImpl/UserImpl.java
@@ -1,13 +1,10 @@
 package com.example.E_commerce.ServiceImpl;
 
-import com.example.E_commerce.Enum.Role;
-import com.example.E_commerce.Entity.UserEntity;
 import com.example.E_commerce.Repository.UserRepository;
 import com.example.E_commerce.Service.UserServiceMain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class UserImpl implements UserServiceMain {
